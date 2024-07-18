1fa05fd56d7016203682fe3b465c89314e856959
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-07-18 15:47:42 +0300

    search bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import "./App.module.css";
+import SearchBar from "../SearchBar/SearchBar";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   useEffect(() => {
     //for http-request
   }, []);
 
+  const handleSearch = (query) => {
+    console.log("search for:", query);
+  };
+
   return (
     <>
       <h1></h1>
+      <SearchBar onSubmit={handleSearch} />
+      <Toaster position="top-center" reverseOrder={false} />
     </>
   );
 }
