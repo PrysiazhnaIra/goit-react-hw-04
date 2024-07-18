aae381f4477ec0c7415c005cf93567740953c62c
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-07-18 14:23:53 +0300

    created components and readme

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.module.css";
+
+function App() {
+  useEffect(() => {
+    //for http-request
+  }, []);
+
+  return (
+    <>
+      <h1></h1>
+    </>
+  );
+}
+
+export default App;
